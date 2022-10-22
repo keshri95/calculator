d60ece034a95262d88eefbb3b07e8c47bd6fbb2d
keshri95 <keshri.ashish1@gmail.com>
2022-10-22 20:19:45 +0530

    ui-done

---
+++ app.js
@@ -0,0 +1,93 @@
+// const Btn = document.querySelector(".btn");
+// const screen = document.querySelector(".screen");
+
+// const allBtns = [
+//   {
+//     id: 1,
+//     name: "1",
+//   },
+//   {
+//     id: 2,
+//     name: "2",
+//   },
+//   {
+//     id: 3,
+//     name: "3",
+//   },
+//   {
+//     id: 4,
+//     name: "4",
+//   },
+//   {
+//     id: 5,
+//     name: "5",
+//   },
+//   {
+//     id: 6,
+//     name: "6",
+//   },
+//   {
+//     id: 7,
+//     name: "7",
+//   },
+//   {
+//     id: 8,
+//     name: "8",
+//   },
+//   {
+//     id: 9,
+//     name: "9",
+//   },
+//   {
+//     id: 10,
+//     name: "10",
+//   },
+//   {
+//     id: 11,
+//     name: "+",
+//   },
+//   {
+//     id: 12,
+//     name: "-",
+//   },
+//   {
+//     id: 13,
+//     name: "*",
+//   },
+//   {
+//     id: 15,
+//     name: "/",
+//   },
+//   {
+//     id: 16,
+//     name: "DEL",
+//   },
+//   {
+//     id: 17,
+//     name: "AC",
+//   },
+//   {
+//     id: 18,
+//     name: "=",
+//   },
+//   {
+//     id: 17,
+//     name: ".",
+//   },
+//   {
+//     id: 18,
+//     name: "0",
+//   },
+// ];
+
+// allBtns.map((btns, id) => {
+//   return (
+//       `<button type="button" class="btn">
+//       ${(btns, id)}
+//       </button>`
+//       );
+// });
+
+// Btn.addEventListener("click", function () {
+//   console.log("map button");
+// });
