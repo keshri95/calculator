89f9e4126c1b7c39dce6e25ad398e4a6f96cd46c
keshri95 <keshri.ashish1@gmail.com>
2022-10-23 13:33:42 +0530

    js-added

--- app.js
+++ app.js
@@ -1,93 +1,20 @@
-// const Btn = document.querySelector(".btn");
-// const screen = document.querySelector(".screen");
+// initalized
+const Btn = document.querySelectorAll(".btn");
+let string = "";
 
-// const allBtns = [
-//   {
-//     id: 1,
-//     name: "1",
-//   },
-//   {
-//     id: 2,
-//     name: "2",
-//   },
-//   {
-//     id: 3,
-//     name: "3",
-//   },
-//   {
-//     id: 4,
-//     name: "4",
-//   },
-//   {
-//     id: 5,
-//     name: "5",
-//   },
-//   {
-//     id: 6,
-//     name: "6",
-//   },
-//   {
-//     id: 7,
-//     name: "7",
-//   },
-//   {
-//     id: 8,
-//     name: "8",
-//   },
-//   {
-//     id: 9,
-//     name: "9",
-//   },
-//   {
-//     id: 10,
-//     name: "10",
-//   },
-//   {
-//     id: 11,
-//     name: "+",
-//   },
-//   {
-//     id: 12,
-//     name: "-",
-//   },
-//   {
-//     id: 13,
-//     name: "*",
-//   },
-//   {
-//     id: 15,
-//     name: "/",
-//   },
-//   {
-//     id: 16,
-//     name: "DEL",
-//   },
-//   {
-//     id: 17,
-//     name: "AC",
-//   },
-//   {
-//     id: 18,
-//     name: "=",
-//   },
-//   {
-//     id: 17,
-//     name: ".",
-//   },
-//   {
-//     id: 18,
-//     name: "0",
-//   },
-// ];
+// logic
 
-// allBtns.map((btns, id) => {
-//   return (
-//       `<button type="button" class="btn">
-//       ${(btns, id)}
-//       </button>`
-//       );
-// });
-
-// Btn.addEventListener("click", function () {
-//   console.log("map button");
-// });
+Array.from(Btn).forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    if (e.target.innerHTML === "=") {
+      string = eval(string);
+      document.querySelector("input").value = string;
+    } else if (e.target.innerHTML === "C") {
+      string = "";
+      document.querySelector("input").value = string;
+    } else {
+      string = string + e.target.innerHTML;
+      document.querySelector("input").value = string;
+    }
+  });
+});
